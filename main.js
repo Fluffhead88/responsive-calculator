6c22e529125b8c370f67c34cf6085313876ba5c8
Fluffhead88 <thigpen.zach@gmail.com>
2018-07-04 14:48:27 -0400

    pushNumber and pushOperator working

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 var calculation = [];
-var number = "";
 
 // function to replace display in html with button that is clicked
 
-function myFunction(x) {
-  document.getElementById("display").textContent = x.target.value;
+function pushNumber() {
+  document.getElementById("display").textContent = this.value;
+  this.value = calculation;
 }
 
 // selects each button clicked and uses function above to put them in place of
 // display in html
+document.querySelectorAll(".num").forEach(element => {
+  element.addEventListener('click', pushNumber);
+});
 
-document.querySelectorAll(".btn").forEach(element => {
-  element.addEventListener('click', myFunction);
-})
-
-
-function pushNumber() {
-  calculation.push(Number(number));
-  number = "";
+// function to replace display in html with button that is clicked
+function pushOperator() {
+  document.getElementById("display").textContent = this.value;
+  this.value = calculation;
 }
+console.log(calculation);
+// selects each operator clicked and uses function above to put them in place of
+// display in html
+document.querySelectorAll(".op").forEach(element => {
+  element.addEventListener('click', pushOperator);
+});
+
+// function to replace display in html with button that is clicked
+function calculate() {
+  document.getElementById("display").textContent = this.value;
+}
+// selects equal clicked and uses function above to put them in place of
+// display in html
+document.querySelectorAll(".eq").forEach(element => {
+  element.addEventListener('click', calculate);
+});
